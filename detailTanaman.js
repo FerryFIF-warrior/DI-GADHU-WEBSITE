ed834309ba8d46f656b878be51ef8238b2549a5e
FIF <ferry_fifyli8@student.uns.ac.id>
2026-04-06 23:24:46 +0700

    Update detailTanaman.js

--- detailTanaman.js
+++ detailTanaman.js
@@ -17,33 +17,55 @@ const container = document.getElementById("detailTanaman");
 
 /*backgrond */
 container.style.backgroundImage = `url(${tanaman.gambar})`;
+container.style.backgroundSize = "cover";
+container.style.backgroundPosition = "center";
 
 container.innerHTML = `
+<div class="w-full flex justify-end">
 
-<div class="max-w-6xl x-full grid md:grid-cols-2 gap-10 items-center">
-<!-- sisi kiri judul -->
-<div class="text-white">
-<h1 class="text-5xl font-bold drop-shadow-lg mb-6">${tanaman.nama}</h1></div>
+<div class="w-[420px] mr-16 relative">
 
-<p class="text-lg bg-black/40 p-4 rounded-lg backdrop-blur-sm">${tanaman.deskripsi}</p>
-</div>
+<!-- PANEL JUDUL -->
+<div class="bg-[#8b6b4c] text-white text-3xl font-bold
+px-6 py-3 rounded-lg shadow-lg
+absolute -top-8 left-6 z-10">${tanaman.nama}</div>
 
-<!-- papan informasi -->
-<div class="bg-[#8b6b4c] text-[#f4f1e6] p-8 rounded-xl shadow-2xl border-4 border-[#6a4f36]">
 
-<h2 class="text-2xl font-bold mb-6 text-center">Informasi Tanaman</h2>
+<!-- PANEL DESKRIPSI / PAPAN KAYU -->
+<div class="pt-14 pb-6 px-6 rounded-xl shadow-2xl border-4 border-[#6a4f36]"
+style="
+background-image:url('resource/kayu.png');
+background-size:cover;
+color:#f4f1e6;
+">
 
-<div class="space-y-3 text-lg">
+<p class="mb-4 text-sm">${tanaman.deskripsi}</p>
+
+<h3 class="font-bold mb-3">Informasi Tanaman</h3>
+
+<div class="space-y-2 text-sm">
 
 <p><b>Suhu Ideal:</b> ${tanaman.suhuIdeal}</p>
-<p><b>Kelembaban:</b> ${tanaman.kelembabanIdeal}</p>
+
+<p><b>Kelembaban:</b> ${tanaman.kelembapanIdeal}</p>
+
 <p><b>Jenis Tanah:</b> ${tanaman.tanah}</p>
+
 <p><b>Musim Tanam:</b> ${tanaman.musim}</p>
 
 </div>
 
-<div class="mt-6 p-4 bg-[#2f5d3a] rounded-lg">
-<b>Keunggulan:</b>${tanaman.keunggulan}</div>
-</div>`;
+
+<!-- KEUNGGULAN -->
+<div class="mt-5 bg-[#2f5d3a] p-3 rounded-md text-sm">
+<b>Keunggulan:</b><p class="mt-1">${tanaman.keunggulan}</p>
+</div>
+
+</div>
+
+</div>
+
+</div>
+`;
 
 }
